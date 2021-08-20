a3a5679cbc3c0dd71d20f0ed116ad7652c4a9506
Luciana Sugeta <lulusugeta@gmail.com>
2021-08-20 13:34:22 -0400

    commit tree 69288b3b59f8af8ecd03ebf08656270edac29856

---
+++ index.js
@@ -0,0 +1,68 @@
+//* Create a function for every eactivity on the list *//
+// function wakeDog() {
+//     console.log("Wake Byron the poddle");
+// }
+
+// function leashDog() {
+//     console.log("Leash Byron the poddle");
+// }
+
+// function walkToPark() {
+//     console.log("Walk to the park with Byron the poddle");
+// }
+
+// function throwFrisbee() {
+//     console.log("Throw the frisbee for Byron the poddle");
+// }
+
+// function walkHome() {
+//     console.log("Walk home with Byron the poddle");
+// }
+
+// function unleashDog() {
+//     console.log("Unleash Byron the poddle");
+// }
+
+//* Generalize and make each function take a dogName and dogBeed parameter *//
+//* Additionally: Each function should return the string that it creates *//
+function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`;
+}
+
+function leashDog(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`;
+}
+
+function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+}
+
+function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`;
+}
+
+function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`;
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+   return routine.map(element => element(dogName, dogBreed))
+};
+// newArray = [];
+// function exerciseDog(dogName, dogBreed) {
+//     routine.map(function(element) {
+//     newArray.push(element(dogName, dogBreed))
+// })};
+// newArray;
